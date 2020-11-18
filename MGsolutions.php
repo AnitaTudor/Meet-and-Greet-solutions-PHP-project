65b791e27b270dd1477eccfeb5b0f170892c1015
Anita Tudor <tudoranita1054@gmail.com>
2020-11-18 18:45:51 +0200

    Added CSS,SassCSS, icons and proper frontend
    - CSS, SassCSS is done by material-kit(creative tim)
    -Update information about app.
    -backup.php is the old html format(without css and frontend) from "tema 1"

--- MGsolutions.php
+++ MGsolutions.php
@@ -1,58 +1,168 @@
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
 <head>
-
+    <title>M&Gsolutions: Meet&Organize online events</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width , initial-scale=1.0">
-    <meta name="description" content="M&Gsolutions">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="keywords" content="Meet,Greet,Solutions,M&Gsolutions,online">
-    <meta charset="UTF-8">
-    <title>
-        M&Gsolutions: Meet&Organize online events
-    </title>
+    <meta name="description" content="M&Gsolutions">
+    <!--  Fonts and icons  -->
+    <!-- Fonts and icons -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+
+    <!-- Material Kit CSS -->
+    <link href="assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
 </head>
+<body><nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg"  color-on-scroll="100">
+    <div class="container">
+        <div class="navbar-translate">
+            <a class="navbar-brand" href="/Meet-and-Greet-solutions-PHP-project/MGsolutions.php?_ijt=e5tn9ni10qed4suvtt62iekbqj#">
+                M&G Solutions </a>
 
-
-<body>
-<main>
-
-    <section class="title-content">
-        <h2> Organize events online using the powerful web app M&Gsolutions: Meet and Greet Solutions</h2>
-        <p>With COVID-19 present and still rising, this web app will allow users to organize online events on different platforms such as discord</p>
-        <p>With M&G solutions app you don't have to install any software, and it will be easy to send out invites via email and direct meeting links to your corporate partners or just personal meetings.</p>
-
-    </section>
-
-    <section class="app-description">
-        <h3>Usage and basic features:</h3>
-        <div class="user-features">
-            <p>Personal account where the user can:</p>
-                <p>Set up a meeting using popular services such as discord </p>
-                <p>Send emails as invite links to promote the meeting</p>
-                <p>Link accounts or friend requests with other users via php server side handling of a data base</p>
-            <p>Meeting/event can be organised as much as the preffered service allows it. For example on discord:</p>
-                <p>The user may set the name of the server created, image of the server, and personal greeting messages as the people join</p>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-        <div class="backend-content">
-            <p>How it works:</p>
-                <p>Data base handling using PHP </p>
-                <p>Secure login for users and safe account link using PHP</p>
-                <p>Create a platform server(discord) using JavaScript and utilising discord API</p>
+
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="dropdown nav-item">
+                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                        <i class="material-icons">apps</i> Register/Login
+                    </a>
+                    <div class="dropdown-menu dropdown-with-icons">
+                        <a href="" class="dropdown-item">
+                            <i class="material-icons">layers</i> Login
+                        </a>
+                        <a href="" class="dropdown-item">
+                            <i class="material-icons">content_paste</i> Register
+                        </a>
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/modusponens112" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.twitch.tv/modusponens112" target="_blank" data-original-title="Follow us on Twitch" rel="nofollow">
+                        <i class="fa fa-twitch"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
-    </section>
-
-    <section class="project-details">
-        <p>GITHUB link : https://github.com/AnitaTudor/Meet-and-Greet-solutions-PHP-project</p>
-        <p>hosting link: https://meet-and-greet-solutions.herokuapp.com/MGsolutions.php</p>
-    </section>
-
-</main>
+    </div>
+</nav>
 
 
-</body>
+<div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/bg8.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 ml-auto mr-auto">
+                <div class="brand text-center">
+                    <h1>M&G Solutions</h1>
+                    <h3 class="title text-center">Meet And Greet Solutions: Your solution during COVID-19.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+<div class="main main-raised">
+    <div class="container">
+        <div class="section text-center">
+            <h2 class="title">Organize events online using the powerful web app M&Gsolutions: Meet and Greet Solutions</h2>
+            <h5 class="description">
+                With COVID-19 present and still rising, this web app will allow users to organize online events on different platforms such as discord
+            </h5>
+        </div>
+    </div>
+    <div class="features">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="info">
+                    <div class="icon icon-info">
+                        <i class="material-icons">chat</i>
+                    </div>
+                    <h4 class="info-title">Chat with your associates</h4>
+                    <p>Using our platform you can create custom chat rooms on powerful apps such as discord.Meetings/events can be organised as much as the preffered service allows it. </p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="info">
+                    <div class="icon icon-success">
+                        <i class="material-icons">verified_user</i>
+                    </div>
+                    <h4 class="info-title">Verified Users</h4>
+                    <p>Create an account today to send/recieve invites to your associates via email. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section text-center">
+        <h2 class="title">Github And Herokuapp Links</h2>
+        <h5 class="description">
+            <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://github.com/AnitaTudor/Meet-and-Greet-solutions-PHP-project" target="_blank" data-original-title="Github Link" rel="nofollow">
+                         <i class="fa fa-github" style="font-size: 60px;"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://meet-and-greet-solutions.herokuapp.com/MGsolutions.php" target="_blank" data-original-title="Herokuapp Link" rel="nofollow">
+                        <i class="fa fa-h-square" style="font-size: 60px;"></i>
+                    </a>
+                     </li>
+            </ul>
+        </h5>
+    </div>
+</div>
+
+<footer class="footer footer-default" >
+    <div class="container">
+        <nav class="float-left">
+            <ul>
+                <li>
+                    <a href="https://www.creative-tim.com/">
+                       Frontend(css) by: Creative Tim
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, M&G Solutions
+        </div>
+    </div>
+</footer>
+<!--   Core JS Files   -->
+<script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+<script src="assets/js/plugins/moment.min.js"></script>
+<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+<script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Google Maps Plugin  -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+<script src="assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script></body>
 </html>
+
 
 
 <?php
---
+++ backup.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta name="viewport" content="width=device-width , initial-scale=1.0">
+    <meta name="description" content="M&Gsolutions">
+    <meta name="keywords" content="Meet,Greet,Solutions,M&Gsolutions,online">
+    <meta charset="UTF-8">
+    <title>
+        M&Gsolutions: Meet&Organize online events
+    </title>
+</head>
+
+
+<body>
+<main>
+
+    <section class="title-content">
+        <h2> Organize events online using the powerful web app M&Gsolutions: Meet and Greet Solutions</h2>
+        <p>With COVID-19 present and still rising, this web app will allow users to organize online events on different platforms such as discord</p>
+        <p>With M&G solutions app you don't have to install any software, and it will be easy to send out invites via email and direct meeting links to your corporate partners or just personal meetings.</p>
+
+    </section>
+
+    <section class="app-description">
+        <h3>Usage and basic features:</h3>
+        <div class="user-features">
+            <p>Personal account where the user can:</p>
+            <p>Set up a meeting using popular services such as discord </p>
+            <p>Send emails as invite links to promote the meeting</p>
+            <p>Link accounts or friend requests with other users via php server side handling of a data base</p>
+            <p>Meeting/event can be organised as much as the preffered service allows it. For example on discord:</p>
+            <p>The user may set the name of the server created, image of the server, and personal greeting messages as the people join</p>
+        </div>
+        <div class="backend-content">
+            <p>How it works:</p>
+            <p>Data base handling using PHP </p>
+            <p>Secure login for users and safe account link using PHP</p>
+            <p>Create a platform server(discord) using JavaScript and utilising discord API</p>
+        </div>
+    </section>
+
+    <section class="project-details">
+        <p>GITHUB link : https://github.com/AnitaTudor/Meet-and-Greet-solutions-PHP-project</p>
+        <p>hosting link: https://meet-and-greet-solutions.herokuapp.com/MGsolutions.php</p>
+    </section>
+
+</main>
+
+
+</body>
+
+</html>
