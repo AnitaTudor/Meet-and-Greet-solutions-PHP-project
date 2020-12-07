f383eb7d335283f03b6dda7f6a822ab4cf4efd6f
Anita Tudor Alexandru <tudoranita1054@gmail.com>
2020-12-07 14:10:48 +0200

    added login and template for php reuse of code(header/footer)

--- MGsolutions.php
+++ MGsolutions.php
@@ -1,69 +1,8 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <title>M&Gsolutions: Meet&Organize online events</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width , initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="keywords" content="Meet,Greet,Solutions,M&Gsolutions,online">
-    <meta name="description" content="M&Gsolutions">
-    <!--  Fonts and icons  -->
-    <!-- Fonts and icons -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+<?php include 'templates/header.php'; ?>
 
-
-    <!-- Material Kit CSS -->
-    <link href="assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
-</head>
-<body><nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg"  color-on-scroll="100">
-    <div class="container">
-        <div class="navbar-translate">
-            <a class="navbar-brand" href="/Meet-and-Greet-solutions-PHP-project/MGsolutions.php">
-                M&G Solutions </a>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="navbar-toggler-icon"></span>
-                <span class="navbar-toggler-icon"></span>
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-
-        <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ml-auto">
-                <li class="dropdown nav-item">
-                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                        <i class="material-icons">apps</i> Register/Login
-                    </a>
-                    <div class="dropdown-menu dropdown-with-icons">
-                        <a href="" class="dropdown-item">
-                            <i class="material-icons">layers</i> Login
-                        </a>
-                        <a href="" class="dropdown-item">
-                            <i class="material-icons">content_paste</i> Register
-                        </a>
-                    </div>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/modusponens4" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
-                        <i class="fa fa-twitter"></i>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.twitch.tv/modusponens112" target="_blank" data-original-title="Follow us on Twitch" rel="nofollow">
-                        <i class="fa fa-twitch"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-
+<body>
 <div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/bg8.jpg')">
     <div class="container">
         <div class="row">
@@ -126,42 +65,10 @@
         </h5>
     </div>
 </div>
-
-<footer class="footer footer-default" >
-    <div class="container">
-        <nav class="float-left">
-            <ul>
-                <li>
-                    <a href="https://www.creative-tim.com/">
-                       Frontend(css) by: Creative Tim
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <div class="copyright float-right">
-            &copy;
-            <script>
-                document.write(new Date().getFullYear())
-            </script>, M&G Solutions
-        </div>
-    </div>
-</footer>
-<!--   Core JS Files   -->
-<script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
-<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
-<script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-<script src="assets/js/plugins/moment.min.js"></script>
-<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-<script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-<script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
-<!--  Google Maps Plugin  -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-<!-- Place this tag in your head or just before your close body tag. -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
-<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-<script src="assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script></body>
+<?php include 'templates/footer.php'; ?>
+</body>
 </html>
+
 
 
 
---
+++ templates/header.php
@@ -0,0 +1,64 @@
+
+<head>
+    <title>M&Gsolutions: Meet&Organize online events</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="keywords" content="Meet,Greet,Solutions,M&Gsolutions,online">
+    <meta name="description" content="M&Gsolutions">
+    <!--  Fonts and icons  -->
+    <!-- Fonts and icons -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+
+    <!-- Material Kit CSS -->
+    <link href="assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
+</head>
+<nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg"  color-on-scroll="100">
+    <div class="container">
+        <div class="navbar-translate">
+            <a class="navbar-brand" href="/Meet-and-Greet-solutions-PHP-project/MGsolutions.php">
+                M&G Solutions </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="dropdown nav-item">
+                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                        <i class="material-icons">apps</i> Register/Login
+                    </a>
+                    <div class="dropdown-menu dropdown-with-icons">
+                        <a href="" class="dropdown-item">
+                            <i class="material-icons">layers</i> Login
+                        </a>
+                        <a href="" class="dropdown-item">
+                            <i class="material-icons">content_paste</i> Register
+                        </a>
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/modusponens4" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.twitch.tv/modusponens112" target="_blank" data-original-title="Follow us on Twitch" rel="nofollow">
+                        <i class="fa fa-twitch"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
